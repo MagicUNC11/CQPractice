8c38766232a0e71d0628e765e189b9bf45793daa
MagicUNC11 <prabhyv@stu.npenn.org>
2022-04-15 21:36:09 -0400

    Made Java Folder Java

---
+++ CodeQuestJava/src/CodeQuestTemplate.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CodeQuestTemplate {
+    public static void main(String[]Args){
+        Scanner scanner = new Scanner(System.in);
+        int testCases = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i<testCases; i++){
+            int vals = Integer.parseInt(scanner.nextLine());
+
+        }
+
+    }
+}
--- CodeQuestJava/src/Prob95ImageCompression.java
+++ CodeQuestJava/src/Prob95ImageCompression.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 
-public class CodeQuestTemplate {
+public class Prob95ImageCompression {
     public static void main(String[]Args){
         Scanner scanner = new Scanner(System.in);
         int testCases = Integer.parseInt(scanner.nextLine());
         for (int i = 0; i<testCases; i++){
             int vals = Integer.parseInt(scanner.nextLine());
             System.out.println(vals);
+            double [] values = new double[vals];
+            for (int i =0;i<)
         }
         System.out.println(testCases);
     }
